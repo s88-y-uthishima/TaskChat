eb607650f40ca6ba3ece06fd5b9cf81c7b47becf
s88-y-uthishima <s88-y-uthishima@users.noreply.github.com>
2020-12-15 17:57:44 +0900

    cardモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_054516) do
+ActiveRecord::Schema.define(version: 2020_12_15_085410) do
+
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "memo"
+    t.bigint "task_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["task_id"], name: "index_cards_on_task_id"
+  end
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -33,5 +42,6 @@ ActiveRecord::Schema.define(version: 2020_12_15_054516) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cards", "tasks"
   add_foreign_key "tasks", "users"
 end
