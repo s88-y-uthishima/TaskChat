3df1cb7ddc7f51b0c6f39300509ffe99579199a6
s88-y-uthishima <s88-y-uthishima@users.noreply.github.com>
2020-12-18 15:12:30 +0900

    チャットに名前の表示追加

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,32 +1,34 @@
 import consumer from "./consumer"
 
-const chatChannel = consumer.subscriptions.create("RoomChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-  },
+$(function() {
+  const chatChannel = consumer.subscriptions.create("RoomChannel", {
+    connected() {
+      // Called when the subscription is ready for use on the server
+    },
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
 
-  received: function(data) {
-    // return  alert(data['message']);
-    return $('#messages').append(data['message']);
-    // Called when there's incoming data on the websocket for this channel
-  },
+    received: function(data) {
+      // return  alert(data['message']);
+      return $('#messages').append(data['message']);
+      // Called when there's incoming data on the websocket for this channel
+    },
 
-  speak: function(message) {
-    return this.perform('speak', {
-      message: message
-    });
-  }
-});
+    speak: function(message) {
+      return this.perform('speak', {
+        message: message
+      });
+    }
+  });
 
 
-$(document).on('keypress', '[data-behavior~=room_speaker]', function(event) {
-  if (event.keyCode === 13) {
-    chatChannel.speak(event.target.value);
-    event.target.value = '';
-    return event.preventDefault();
-  }
+  $(document).on('keypress', '[data-behavior~=room_speaker]', function(event) {
+    if (event.keyCode === 13) {
+      chatChannel.speak(event.target.value);
+      event.target.value = '';
+      return event.preventDefault();
+    }
+  });
 });
