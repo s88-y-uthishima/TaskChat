c2a8980f5b46225692680656bc167ba342bb5c60
s88-y-uthishima <s88-y-uthishima@users.noreply.github.com>
2020-12-18 17:51:08 +0900

    ユーザー詳細、現在位置の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'top#index' 
-
+  resources :users, only: :show
   resources :task, only: [:new, :create, :edit, :update, :destroy] do
     resources :card, except: :index
   end
